32828241a64ec1eca0359f0b70aa3ff29f2aa772
tushark10042001 <tushar.k10042001@gmail.com>
2026-09-02 21:34:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,30 @@
-from fastapi import FastAPI, Form
+from fastapi import FastAPI, Form, Request
 from fastapi.responses import FileResponse, HTMLResponse
+from fastapi.templating import Jinja2Templates 
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory = '.')
+
 @app.get('/')
-def home():
-  return FileResponse("index.html")
+def home(request: Request):
+  return templates.TemplateResponse(
+    'index.html',
+    {'request' : request}
+  )
+    
    
 @app.post('/predict')
 def predict(
+  request: Request,
   number_1: float = Form(...)
 ):
   prediction = number_1 * 10
 
-  return HTMLResponse(content = f"""
-  <!DOCTYPE html>
-  
-  <html>
-  
-  <head>
-     <title>Prediction</title>
-  </head>
-
-  <body>
-
-  <p>Your prediction is: {prediction}</p>
-  
-  </body>
-  </html>
-  """)
-  
+  return templates.TemplateResponse(
+    'results.html',
+    {
+      'request' : request,
+      'prediction' : prediction
+    }
+  )
