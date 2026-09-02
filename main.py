5beb6a6e995ffdfcda758eb7ad8620579c62fa67
tushark10042001 <tushar.k10042001@gmail.com>
2026-09-02 20:28:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI
-from fastapi.responses import FileResponse
+from fastapi import FastAPI, Form
+from fastapi.responses import FileResponse, HTMLResponse
 
 app = FastAPI()
 
@@ -7,3 +7,26 @@ app = FastAPI()
 def home():
   return FileResponse("index.html")
    
+@app.post('/predict')
+def predict(
+  number_1: float = Form(...)
+):
+  prediction = number_1 * 10
+
+  return """
+  <!DOCTYPE html>
+  
+  <html>
+  
+  <head>
+     <title>Prediction</title>
+  </head>
+
+  <body>
+
+  <p>Your prediction is: {prediction}</p>
+  
+  </body>
+  </html>
+  """
+  
